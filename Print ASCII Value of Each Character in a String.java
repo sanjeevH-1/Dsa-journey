9260b5a084410d1666ae6187da43e4133b3cbe1d
sanjeev hanchinal <sanjeev.hanchinal1@gmail.com>
2025-05-30 08:57:53 +0530

    .

---
+++ Print ASCII Value of Each Character in a String.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        String str = "sanjeev";
+        char[] ch = str.toCharArray();
+        for(char c : ch){
+            System.out.println((int)c);
+        }
+        
+    }
+}
