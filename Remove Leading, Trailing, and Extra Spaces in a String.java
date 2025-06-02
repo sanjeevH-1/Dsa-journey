2de41861faa3a27bc011477d08cd5b2aaccff039
sanjeev hanchinal <sanjeev.hanchinal1@gmail.com>
2025-06-02 09:40:15 +0530

    .

---
+++ Remove Leading, Trailing, and Extra Spaces in a String.java
@@ -0,0 +1,9 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        String str=" Sanj eev ";
+        System.out.println(str.replace(" ", ""));
+    }
+}
