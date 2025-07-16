15ca6189a88dedf8fe3de7d5e4197b5d55431c38
sanjeev hanchinal <sanjeev.hanchinal1@gmail.com>
2025-07-16 09:10:05 +0530

    https://neetcode.io/problems/merge-sorted-array?list=neetcode250

--- Merge Sorted Array.java
+++ Merge Sorted Array.java
@@ -3,12 +3,10 @@ class Solution {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int start = nums1.length - m;
+        int j = 0;
         for (int i = m; i < nums1.length; i++) {
+            nums1[i] = nums2[j++];
 
-            for (int j = 0; j < nums2.length; j++) {
-                nums1[i++] = nums2[j];
-
-            }
         }
         Arrays.sort(nums1);
         System.out.println(nums1);
